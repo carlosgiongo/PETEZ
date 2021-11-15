687524509d33e448684fa3e7076581bc91c2c388
Isabela Maria <isabeladacampo@gmail.com>
2021-11-15 14:58:58 -0300

    finalizado footer

--- footer.php
+++ footer.php
@@ -1,41 +1,69 @@
 <div class="container bg-dark">
   <footer class="row row-cols-5 py-5 my-5 border-top">
-    <div class="col">
-      <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
-        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-      </a>
-    </div>
-
-    <div class="col start">
-        <img src="<?= IMG_PATH ?>/logo_petez.svg" class="img-fluid" alt="...">
-    </div>
-
-    <div class="col">
-      <ul class="nav flex-column">
-        <li class="nav-item text-muted"><h6>Endereço</h6></li>
-        <li class="nav-item text-muted">Avenida Nereu Ramos, 3777-D, Bairro Seminário, Chapecó</li>
-        <li class="nav-item text-muted"><h6>Telefone</h6></li>
-        <li class="nav-item text-muted">(99)99999-9999</li>
-        <li class="nav-item text-muted"><h6>E-mail</h6></li>
-        <li class="nav-item text-muted">petez@petez.com.br</li>
-      </ul>
-    </div>
-    <div class="col">
-      <ul class="nav flex-column">
-        <li class="nav-item"><a href="#" class="nav-link p-0 text-muted">Política de Troca</a></li>
-        <li class="nav-item"><a href="#" class="nav-link p-0 text-muted">Sobre nós</a></li>
-        <li class="nav-item"><p class="text-muted">PETEZ © 2021</p></li>
-      </ul>
-    </div>
-    <div class="col">
-      <ul class="nav flex-column">
-        <li class="nav-item text-muted"><h6>Pagamentos</h6></li>
-        <!-- <li class="nav-item">Avenida Nereu Ramos, 3777-D, Bairro Seminário, Chapecó</li>
-        <li class="nav-item"><h6>Telefone</h6></li>
-        <li class="nav-item">(99)99999-9999</li>
-        <li class="nav-item"><h6>E-mail</h6></li>
-        <li class="nav-item">petez@petez.com.br</li> -->
-      </ul>
+    <div class="row col-12">
+      <div class="col col-sm-3 start d-none d-sm-block">
+        <img src="<?= IMG_PATH ?>/logo_petez.svg" class="img-fluid" alt="logo PETEZ">
+      </div>
+      <div class="col col-sm-3">
+        <ul class="nav flex-column">
+          <li class="nav-item text-muted"><h6>Endereço</h6></li>
+          <li class="nav-item text-muted">Avenida Nereu Ramos, 3777-D, Bairro Seminário, Chapecó</li>
+          <li class="nav-item text-muted"><h6>Telefone</h6></li>
+          <li class="nav-item text-muted">(99)99999-9999</li>
+          <li class="nav-item text-muted"><h6>E-mail</h6></li>
+          <li class="nav-item text-muted">petez@petez.com.br</li>
+        </ul>
+      </div>
+      <div class="col col-sm-3">
+        <ul class="nav flex-column">
+          <li class="nav-item"><a href="#" class="nav-link p-0 text-muted">Política de Troca</a></li>
+          <li class="nav-item"><a href="#" class="nav-link p-0 text-muted">Sobre nós</a></li>
+          <li class="nav-item"><p class="text-muted">PETEZ © 2021</p></li>
+        </ul>
+      </div>
+      <div class="col col-sm-3">
+        <ul class="nav flex-column">
+          <li class="nav-item text-muted"><h6>Pagamentos</h6></li>
+        </ul>
+        <div class="row">
+          <div class="col">
+            <i class="fab fa-cc-mastercard fa-lg"></i>
+          </div>
+          <div class="col">
+            <i class="fab fa-cc-visa fa-lg"></i>
+          </div>
+          <div class="col">
+            <i class="fab fa-cc-stripe fa-lg"></i>
+          </div>
+          <div class="col">
+            <i class="fab fa-cc-stripe fa-lg"></i>
+          </div>
+          <div class="col">
+            <i class="fab fa-cc-paypal fa-lg"></i>
+          </div>
+          <div class="col">
+            <i class="fab fa-cc-jcb fa-lg"></i>
+          </div>
+          <div class="col">
+            <i class="fab fa-cc-discover fa-lg"></i>
+          </div>
+          <div class="col">
+            <i class="fab fa-cc-diners-club fa-lg"></i>
+          </div>
+          <div class="col">
+            <i class="fab fa-cc-apple-pay fa-lg"></i>
+          </div>
+          <div class="col">
+            <i class="fab fa-cc-amex fa-lg"></i>
+          </div>
+          <div class="col">
+            <i class="fab fa-cc-apple-pay fa-lg"></i>
+          </div>
+          <div class="col">
+            <i class="fab fa-cc-amazon-pay fa-lg"></i>
+          </div>
+        </div>
+      </div>
     </div>
   </footer>
 </div>
