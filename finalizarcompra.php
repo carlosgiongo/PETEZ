e7b63587281813655b509206dfca4a4d5495f108
carlosgiongo <carlosgiongo@users.noreply.github.com>
2021-12-03 19:00:49 -0300

    Ajustes

--- finalizarcompra.php
+++ finalizarcompra.php
@@ -1,5 +1,26 @@
 <?php
 include_once "header.php";
-echo "Pagamento aqui";
+?>
+<script>
+    function concluirPagamento(){
+        window.location.href = "./concluido.php"    
+    }
+</script>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col d-flex justify-content-center">
+                <div class="text-center">
+                    <h1>Realizar pagamento</h1>
+                    <input class="form-control" type="text" placeholder="Nome completo">
+                    <input class="form-control" type="text" placeholder="Numero do cartao">
+                    <input class="form-control" type="text" placeholder="Codigo de segurança">
+                    <button class="btn btn-outline-dark" style="margin: 10px 0;" onclick="concluirPagamento()">Concluir</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<?php
 include_once "footer.php";
 ?>
